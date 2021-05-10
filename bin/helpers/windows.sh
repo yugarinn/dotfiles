ebf89e77b2eb1dce0e606ddcb32d87312c56f20c
Sergio Uve <sergiouve@gmail.com>
2021-05-11 00:21:16 +0200

    Add multimonitor support for rofi window switcher

--- bin/helpers/windows.sh
+++ bin/helpers/windows.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Get available windows
-windows=$(swaymsg -t get_tree | jq -r '.nodes[1].nodes[].nodes[] | .. | (.id|tostring) + " " + .name?' | grep -e "[0-9]* ."  )
-
+WINDOWS=$(swaymsg -t get_tree | jq -r 'recurse(.nodes[]?)|recurse(.floating_nodes[]?)|select(.type=="con"),select(.type=="floating_con")|(.id|tostring)+" "+.app_id+": "+.name')
 # Select window with rofi
-selected=$(echo "$windows" | rofi -dmenu -i | awk '{print $1}')
+SELECTED=$(echo "$WINDOWS" | rofi -dmenu -i | awk '{print $1}')
+
+
 
 # Tell sway to focus said window
-swaymsg [con_id="$selected"] focus
+swaymsg [con_id="$SELECTED"] focus
