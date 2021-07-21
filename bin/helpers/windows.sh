9594ac6efcf48cb163f531f9931d73157f72276b
Sergio Uve <sergiouve@gmail.com>
2021-07-21 09:37:04 +0200

    Update

--- bin/helpers/windows.sh
+++ bin/helpers/windows.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# Get available windows
+SCREEN_POS=$(swaymsg -t get_outputs | jq -r '.[] | select(.focused).rect | "\(.width)x\(.height)\\+\(.x)\\+\(.y)"')
+XWAYLAND_OUTPUT=$(xrandr | rg -oP "[A-Z]+[0-9]+(?= [a-z]+ $SCREEN_POS)")
+MONITOR_ID=$(rofi --help | rg $XWAYLAND_OUTPUT -B1 | sed -sr '/ID/!d;s/[^:]*:\s([0-9])/\1/')
+
 WINDOWS=$(swaymsg -t get_tree | jq -r 'recurse(.nodes[]?)|recurse(.floating_nodes[]?)|select(.type=="con"),select(.type=="floating_con")|(.id|tostring)+" "+.app_id+": "+.name')
-# Select window with rofi
-SELECTED=$(echo "$WINDOWS" | rofi -dmenu -i | awk '{print $1}')
+SELECTED=$(echo "$WINDOWS" | rofi -dmenu -i -m $MONITOR_ID | awk '{print $1}')
 
-
-
-# Tell sway to focus said window
 swaymsg [con_id="$SELECTED"] focus
