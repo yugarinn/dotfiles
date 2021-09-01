86f540599d970a9d90c0d90ec7d4b15b30985751
Sergio Uve <sergiouve@gmail.com>
2021-09-01 23:01:11 +0200

    Update

--- install.sh
+++ install.sh
@@ -3,41 +3,39 @@
 git --version 2>&1 >/dev/null
 GIT_IS_AVAILABLE=$?
 
-if [ $GIT_IS_AVAILABLE -eq 0 ]; then
+if [ $GIT_IS_AVAILABLE -ne 0 ]; then
   echo 'installing git...'
   sudo dnf install git -y
   echo 'installing git... done'
 fi
 
-git clone --depth 1 https://github.com/sergiouve/dotfiles ~/.dotfiles
+[[ -d ~/.dotfiles ]] || git clone --depth 1 https://github.com/sergiouve/dotfiles ~/.dotfiles
 
-while true; do
-  read -p "Install software and update? (y/n) " yn
-  case $yn in
-    [Yy]* ) ~/.dotfiles/bin/bootstrap.sh; break;;
-    [Nn]* ) break;;
-    * ) echo "C'mon you can do this";;
-  esac
+PS3='Please enter your choice: '
+options=("Install software and update" "Setup system config" "Setup dotfiles and symlinks" "Quit")
+
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Option 1")
+            echo "Installing and updating software..."
+            sleep 3
+            ~/.dotfiles/bin/bootstrap.sh;
+            ;;
+        "Option 2")
+            echo "Setting up system config..."
+            sleep 3
+            ~/.dotfiles/bin/configs.sh;
+            ;;
+        "Option 3")
+            echo "Setting up dotfiles and symlinks..."
+            sleep 3
+            ~/.dotfiles/bin/dotfiles.sh;
+            ;;
+        "Quit")
+            echo "Now that's what I call a dead parrot."
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
 done
-
-# Config
-while true; do
-  read -p "Setup system configs? (y/n) " yn
-  case $yn in
-    [Yy]* ) ~/.dotfiles/bin/configs.sh; break;;
-    [Nn]* ) break;;
-    * ) echo "C'mon you can do this";;
-  esac
-done
-
-# Dotfiles
-while true; do
-  read -p "Setup dotfiles? (y/n) " yn
-  case $yn in
-    [Yy]* ) ~/.dotfiles/bin/dotfiles.sh; break;;
-    [Nn]* ) break;;
-    * ) echo "C'mon you can do this";;
-  esac
-done
-
-echo "Now that's what I call a dead parrot."
