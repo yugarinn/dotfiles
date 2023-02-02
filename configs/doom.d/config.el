a42fa7805f385d859e942088c5a74e3e032754c6
Sergio Uve <sergiouve@gmail.com>
2023-02-02 16:51:40 +0100

    Update

--- configs/doom.d/config.el
+++ configs/doom.d/config.el
@@ -1,107 +1,48 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
+;; Me
 (setq user-full-name "yugarinn"
       user-mail-address "yugarinn@proton.me")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "Source Code Pro" :size 11)
-      doom-variable-pitch-font (font-spec :family "Source Code Pro" :size 12))
-;; Big font mode
+;; Fonts
+(setq doom-font (font-spec :family "Source Code Pro" :size 12)
+      doom-variable-pitch-font (font-spec :family "Source Code Pro" :size 12 :weight 'semi-bold))
 (setq doom-big-font (font-spec :family "Source Code Pro" :size 19))
 
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
+;; UI
 (setq doom-theme 'doom-one)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type 'relative)
 
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
-
-;; Web mode
-(defun my/web-mode-hook ()
-  (setq web-mode-markup-indent-offset 4
-        web-mode-code-indent-offset 2
-        tab-width 2
-        c-basic-offset 2))
-
-(setq web-mode-script-padding 0)
-(setq web-mode-style-padding 0)
-
-(add-hook 'web-mode-hook 'my/web-mode-hook)
-
-;; JS mode
-(setq js-indent-level 2)
-(setq js2-basic-offset 2)
-
-;; Rust mode
-(defun my/rust-mode-hook()
-  (setq flycheck 1))
-
-(add-hook 'rust-mode-hook 'my/rust-mode-hook)
-
-;; Org
-(setq org-directory "~/Dropbox/org/")
-(setq org-journal-dir "~/Dropbox/org/journal/")
-
-;; Do not cache non existing file in projectile
-(defadvice! dont-cache-if-file-doesnt-exist-a (&rest _)
-  :before-until #'projectile-cache-files-find-file-hook
-  (and buffer-file-name (file-exists-p buffer-file-name)))
-
-;; FIXME Fix for emacs 27
-;; https://github.com/emacs-lsp/lsp-mode/issues/1778
-(setq lsp-gopls-codelens nil)
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+(add-hook 'window-setup-hook #'toggle-frame-maximized)
 
 ;; Modeline
 (setq doom-modeline-vcs-max-length 120)
-
-;; Coffe Mode
-(custom-set-variables '(coffee-tab-width 2))
+(setq doom-modeline-modal-icon nil)
 
 ;; Vertico
 (after! vertico
-  (setq vertico-posframe-height 30)
+  (setq vertico-posframe-height 31)
   (setq vertico-count 30))
 
 (setq vertico-posframe-parameters
       '((left-fringe . 2)
         (right-fringe . 2)))
+
+(vertico-multiform-mode)
+
+;; Sorting folders before files and by alpha order on find-file buffer
+(setq vertico-multiform-commands
+      '((describe-symbol (vertico-sort-function . vertico-sort-alpha))))
+
+(setq vertico-multiform-categories
+      '((symbol (vertico-sort-function . vertico-sort-alpha))
+        (file (vertico-sort-function . sort-directories-first))))
+
+(defun sort-directories-first (files)
+  (setq files (vertico-sort-alpha files))
+  (nconc (seq-filter (lambda (x) (string-suffix-p "/" x)) files)
+         (seq-remove (lambda (x) (string-suffix-p "/" x)) files)))
 
 ;; Tabnine
 (after! company
@@ -141,3 +82,52 @@
 (after! modus-themes
   (setq modus-themes-tabs-accented t)
   (setq modus-themes-mode-line '(borderless)))
+
+;; Org
+(defun my/set-org-margins()
+  (setq left-margin-width 20)
+  (setq right-margin-width 20)
+  (set-window-buffer nil (current-buffer)))
+
+(add-hook 'org-mode-hook (lambda ()
+                           (my/set-org-margins)
+                           (linum-mode -1)))
+
+;; Treemacs
+(after! treemacs
+  (treemacs-add-and-display-current-project-exclusively)
+  (treemacs-project-follow-mode 1)
+  (treemacs-follow-mode 1)
+  (treemacs-git-mode 'deferred)
+  (setq treemacs-width 45))
+
+;; Web mode
+(defun my/web-mode-hook ()
+  (setq web-mode-markup-indent-offset 4
+        web-mode-code-indent-offset 2
+        tab-width 2
+        c-basic-offset 2))
+
+(setq web-mode-script-padding 0)
+(setq web-mode-style-padding 0)
+
+(add-hook 'web-mode-hook 'my/web-mode-hook)
+
+;; JS mode
+(setq js-indent-level 2)
+(setq js2-basic-offset 2)
+
+;; Rust mode
+(defun my/rust-mode-hook()
+  (setq flycheck 1))
+
+(add-hook 'rust-mode-hook 'my/rust-mode-hook)
+
+;; Do not cache non existing file in projectile
+(defadvice! dont-cache-if-file-doesnt-exist-a (&rest _)
+  :before-until #'projectile-cache-files-find-file-hook
+  (and buffer-file-name (file-exists-p buffer-file-name)))
+
+;; Coffe Mode
+(custom-set-variables '(coffee-tab-width 2))
+
