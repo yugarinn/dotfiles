d905e8b97881eec8dce13a0d2ddfed7b8f891867
Sergio Uve <sergiouve@gmail.com>
2023-08-22 18:07:33 +0200

    Update

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -4,13 +4,14 @@ sudo dnf upgrade -y
 
 # Add third party repositories
 sudo rpm -ivh http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-38.noarch.rpm
+sudo dnf copr enable solopasha/hyprland -y
 
 # Installing the basics
 echo 'installing software...'
 
 set -x \
     && sudo dnf upgrade \
-    && sudo dnf install -y tmux zsh mako pavucontrol
+    && sudo dnf install -y tmux zsh mako pavucontrol hyprland
 
 # Installing from Flathub
 flatpak install flathub md.obsidian.Obsidian com.spotify.Client org.mozilla.Thunderbird -y
