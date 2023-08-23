24c00d365de585ce279574018a0893523a1c66fb
Sergio Uve <sergiouve@gmail.com>
2023-08-23 15:24:05 +0200

    Add mako config

--- bin/dotfiles.sh
+++ bin/dotfiles.sh
@@ -8,6 +8,7 @@ ln -s ~/.dotfiles/configs/hypr/waybar ~/.config/waybar
 
 ln -s ~/.dotfiles/configs/doom ~/.config/doom
 ln -s ~/.dotfiles/configs/tmux/tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/configs/mako ~/.config/mako
 ln -s ~/.dotfiles/configs/profile ~/.profile
 ln -s ~/.dotfiles/configs/zshrc ~/.zshrc
 ln -s ~/.dotfiles/configs/tmux/tmux ~/.tmux.conf
